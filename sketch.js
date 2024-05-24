2bc324bb1e64af17e33a5d2dee0a06de8f7e4ac2
pmarathi314 <pmarathi314@gmail.com>
2024-05-24 17:19:54 -0500

    initialize repo

---
+++ sketch.js
@@ -0,0 +1,10 @@
+let flock
+
+function setup() {
+    createCanvas(windowWidth, windowHeight - 10);
+}
+
+function draw() {
+    background(windowWidth, windowHeight - 10);
+    ellipse(200, 200, 50, 50);
+}
