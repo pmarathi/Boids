196a768070301bad9d9496fb8c7efb71e5089016
pmarathi314 <pmarathi314@gmail.com>
2024-05-26 18:29:50 -0500

    multiple boids can be rendered

--- sketch.js
+++ sketch.js
@@ -4,13 +4,14 @@ let flock
 // prepares p5 resources for rendering
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    
     // createCanvas(400, 400);
-    flock = new Flock(1, 10);
+    flock = new Flock(2, 10);
 }
 
 //renders the graphics
 function draw() {
     background(220);
-    flock.render();
     flock.update();
+    flock.render();
 }
